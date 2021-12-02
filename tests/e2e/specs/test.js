d8a8a8bc6c792ad2a06b8958393fd72d78ea12ba
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2021-12-03 00:05:50 +0900

    ページタイトルのテストを追加

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,4 +1,11 @@
-// https://docs.cypress.io/api/introduction/api.html
+describe('メニューバー', () => {
+  it('ページタイトルが正しく表示されていること', () => {
+    cy.visit('/')
+    cy.contains('.v-toolbar__title', '都道府県別人口構成')    
+  })
+  
+})
+
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
