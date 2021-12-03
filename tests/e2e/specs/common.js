5eebdeab5a3c2984120af51755470db3cc6ea15e
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2021-12-03 21:47:42 +0900

    メニューバーのE2Eテストを追加

--- tests/e2e/specs/common.js
+++ tests/e2e/specs/common.js
@@ -1,7 +1,24 @@
 describe('メニューバー', () => {
-  it('ページタイトルが正しく表示されていること', () => {
+  it('メニューバーに必要な情報が表示されていること', () => {
     cy.visit('/')
+    cy.get('.v-app-bar')
+    cy.get('.v-app-bar__nav-icon')
     cy.contains('.v-toolbar__title', '都道府県別人口構成')    
   })
-  
+  it('ナビゲーションドロワーに必要な情報が表示されていること', () => {
+    // 「このアプリについて」ボタン
+    cy.visit('/')
+    cy.get('.v-app-bar__nav-icon').click()
+    cy.get('.v-list-item__title').contains('このアプリについて').click()
+    cy.get('.v-card__title').contains('このアプリについて')
+    cy.get('#overview-dialog-close-button').contains('OK').click()
+
+    // GitHubリンク
+    cy.get('.v-app-bar__nav-icon').click()
+    cy.get('.v-list-item__title').contains('GitHub')
+    cy.get('.v-list-item-group a')
+      .first()
+      .should('have.attr', 'href', 'https://github.com/Mizuki901/population-composition')
+      .should('have.attr', 'target', '_blank')
+  })
 })
