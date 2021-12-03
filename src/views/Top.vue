31a45fce0e12d8a4ab70a38f938e9250d41df2dd
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2021-12-03 23:13:33 +0900

    トップページを仮実装

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,15 +1,53 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row class="text-center mt-10">
+      <v-col>
+        <span>
+          人口構成を表示したい都道府県やその市区町村を選択してください。
+        </span>
+      </v-col>
+    </v-row>
+    <v-row class="mt-10" justify="center">
+      <!-- 都道府県のセレクトボックス -->
+      <v-col
+        cols="4"
+      >
+        <v-select
+          :items="prefectures"
+          label="都道府県"
+          outlined
+        ></v-select>
+      </v-col>
+      <!-- 市区町村のセレクトボックス -->
+      <v-col
+        cols="4"
+      >
+        <v-select
+          :items="cities"
+          label="市区町村"
+          outlined
+        ></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Top',
 
-    components: {
-      HelloWorld,
-    },
+    data: () => ({
+      prefectures: [
+        '千葉',
+        '東京',
+        '神奈川',
+      ],
+      cities: [
+        '渋谷区',
+        '目黒区',
+        '品川区',
+        '大田区',
+      ]
+    })
   }
 </script>
--- tests/e2e/specs/top.js
+++ tests/e2e/specs/top.js
@@ -2,12 +2,19 @@ describe('トップページ', () => {
   describe('ルーティング', () => {
     it('ルートURL指定時に、トップページが表示されること', () => {
       cy.visit('/')
-      cy.contains('h1', 'Welcome to Vuetify')
+      cy.contains('span', '人口構成を表示したい都道府県やその市区町村を選択してください。')
     })
     it('ルート以外の任意のURL指定時に、トップページにリダイレクトされること', () => {
       cy.visit('/#/xxx')
       cy.url().should('not.include', '/#/xxx')
-      cy.contains('h1', 'Welcome to Vuetify')
+      cy.contains('span', '人口構成を表示したい都道府県やその市区町村を選択してください。')
+    })
+  })
+  describe('コンテンツ', () => {
+    it('必要な要素が表示されていること', () => {
+      cy.visit('/')
+      cy.get('.v-select__slot label').contains('都道府県')
+      cy.get('.v-select__slot label').contains('市区町村')
     })
   })
 })
