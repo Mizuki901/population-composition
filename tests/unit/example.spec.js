c1b166106fbd8c29c4592cdb062bb6f8bf5882ab
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2021-12-06 20:46:35 +0900

    不要なファイルの削除し忘れ

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Top from '@/components/Top.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Top.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Top, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
